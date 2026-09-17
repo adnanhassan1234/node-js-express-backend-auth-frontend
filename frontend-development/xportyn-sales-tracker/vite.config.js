c86a1e092f42d69f4dc35df2b647bb4cf1c094bf
ahassan <ah5404219@gmail.com>
2026-09-17 19:00:14 +0500

    xportyn frotned and bacnd modify

--- frontend-development/xportyn-sales-tracker/vite.config.js
+++ frontend-development/xportyn-sales-tracker/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5170,
     open: true,
     /**
      * Dev proxy: frontend `/api/...` call karta hai aur Vite usay
